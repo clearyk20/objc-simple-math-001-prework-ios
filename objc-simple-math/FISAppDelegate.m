470dccd49d260c452d3a73d03c44ca499d4cb0fb
clearyk20 <clearyk20@outlook.com>
2016-02-23 23:28:08 -0500

    Learning math with objective-c

--- objc-simple-math/FISAppDelegate.m
+++ objc-simple-math/FISAppDelegate.m
@@ -7,11 +7,57 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
-    /**
-     
-     * Write your code here.
-     
-     */
+    NSInteger i = 0;
+    NSLog(@"i: %li", i);
+    
+    i = 1 + 1;
+    NSLog(@"i: %li", i);
+    
+    i = 5 - 3;
+    NSLog(@"i: %li", i);
+    i = 2 * 3 ;
+    NSLog(@"i: %li", i);
+    i = 12 / 2;
+    NSLog(@"i: %li", i);
+    i = 10 % 11;
+    NSLog(@"i: %li", i);
+    
+    NSInteger a = 0;
+    NSInteger b = 0;
+    NSInteger c = a + b;
+    NSLog(@"a: %li b: %li c: %li", a, b, c);
+    
+    a = 17;
+    b = 29;
+    c = a + b;
+    NSLog(@"a: %li b: %li c: %li", a, b, c);
+    
+    c = a - b;
+    NSLog(@"a: %li b: %li c: %li", a, b, c);
+    
+    c = a * b;
+    NSLog(@"a: %li b: %li c: %li", a, b, c);
+
+    c = a / b;
+    NSLog(@"a: %li b: %li c: %li", a, b, c);
+    
+    NSUInteger u = 1;
+    NSLog(@"u: %lu", u);
+    
+    u = 2 + 3;
+    NSLog(@"u: %lu", u);
+    
+    u = 2 * 3;
+    NSLog(@"u: %lu", u);
+    
+    u = -1;
+    NSLog(@"u: %lu", u);
+    
+    u = 8 - 10;
+    NSLog(@"u: %lu", u);
+    
+
+    
     
     // do not alter
     return YES;  //
